469bdcc5e0e95e97fa597469179d209f988ac967
Oreo <oreo@noemail.com>
2020-11-09 08:36:33 -0500

    Add project files.

---
+++ Invisbar/Explorer Fix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace Invisbar
+{
+    class Explorer_Fix
+    {
+        public static void restart()
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("explorer"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+
+        public static void regkey(string keypath, string keyname, string keyvalue)
+        {
+            Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(keypath);
+            key.SetValue(keyname, keyvalue, RegistryValueKind.DWord);
+            key.Close();
+        }
+    }
+}
---
+++ Invisbar/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Invisbar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Invisibar, Created By Oreo";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Select A Option For Your Taskbar:\n");
+            Console.WriteLine("1. Completely Invisible");
+            Console.WriteLine("2. Slightly Visible");
+            Console.WriteLine("3. Default Taskbar\n");
+            string Transparency = Console.ReadLine();
+
+            Console.Clear();
+
+            if (Transparency == "1")
+            {
+                //completly invisible
+                Console.WriteLine("Editing Taskbar...\n");
+                //set transparency effects on
+                Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize", "EnableTransparency", "1");
+                //set invisible taskbar on
+                Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "TaskbarAcrylicOpacity", "0");
+                //restart explorer
+                Explorer_Fix.restart();
+
+            }
+            if (Transparency == "2")
+            {
+                //slightly invisible
+                Console.WriteLine("Editing Taskbar...\n");
+                //transparency effects on
+                Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize", "EnableTransparency", "1");
+                //invisible taskbar off
+                Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "TaskbarAcrylicOpacity", "999");
+                //restart explorer
+                Explorer_Fix.restart();
+            }
+            if (Transparency == "3")
+            {
+                Console.WriteLine("Editing Taskbar...\n");
+                //transparency effects off
+                Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize", "EnableTransparency", "0");
+                //invisible taskbar off
+                Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "TaskbarAcrylicOpacity", "999");
+            }
+
+            Console.WriteLine("Finished!\nPress Enter To Close This Window");
+            Console.ReadLine();
+        }
+    }
+}
