afb66454bd2b0a0071674aaab5a2221a93d4b26c
admin <forth@riseup.net>
2022-03-14 02:29:02 -0400

    Small code improvements, Improved GUI, Removed clock option

--- Invisbar/Program.cs
+++ Invisbar/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace Invisbar
@@ -10,8 +9,9 @@ namespace Invisbar
         {
             while(true)
             {
-                Console.Title = "Invisbar";
-                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetWindowSize(42, 20);
+                Console.Title = "Invisbar | github.com/Africanized";
+                Console.ForegroundColor = ConsoleColor.Magenta;
 
                 Console.WriteLine("Visibility Options:");
                 Console.WriteLine("------------------------------");
@@ -19,78 +19,57 @@ namespace Invisbar
                 Console.WriteLine("2. Slightly Visible");
                 Console.WriteLine("3. Default Taskbar Visibility");
                 Console.WriteLine("------------------------------");
-                Console.WriteLine("\nMinor Tweaks:");
+                Console.WriteLine("Other Options:");
                 Console.WriteLine("------------------------------");
-                Console.WriteLine("4. Show Seconds On Clock");
-                Console.WriteLine("5. Reset All To Defaults");
+                Console.WriteLine("4. Reset All To Defaults");
+                Console.WriteLine("0. Exit This Application");
                 Console.WriteLine("------------------------------");
-                Console.WriteLine("\nOther Options:");
-                Console.WriteLine("------------------------------");
-                Console.WriteLine("6. Exit This Application");
-                Console.WriteLine("------------------------------");
-                Console.Write("Choose A Option For Your Taskbar: ");
+                Console.Write("Select An Option: ");
                 int userInput = int.Parse(Console.ReadLine());
 
-                Console.Clear();
-
+                //Change to "Completely Invisible".
                 if (userInput == 1)
                 {
-                    //completly invisible
                     Console.WriteLine("Editing Taskbar...\n");
-                    //set transparency effects on
                     Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize", "EnableTransparency", "1");
-                    //set invisible taskbar on
                     Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "TaskbarAcrylicOpacity", "0");
                 }
+                //Change to "Slightly Invisible".
                 else if (userInput == 2)
                 {
-                    //slightly invisible
                     Console.WriteLine("Editing Taskbar...\n");
-                    //transparency effects on
                     Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize", "EnableTransparency", "1");
-                    //invisible taskbar off
                     Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "TaskbarAcrylicOpacity", "999");
-                    //restart explorer
                 }
+                //Change the taskbar back to default.
                 else if (userInput == 3)
                 {
                     Console.WriteLine("Editing Taskbar...\n");
-                    //transparency effects off
                     Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize", "EnableTransparency", "0");
-                    //invisible taskbar off
                     Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "TaskbarAcrylicOpacity", "999");
                 }
+                //Disable Everything.
                 else if (userInput == 4)
                 {
                     Console.WriteLine("Editing Taskbar...\n");
-                    //add seconds regkey
-                    Explorer_Fix.regkey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "ShowSecondsInSystemClock", "1");
-                }
-                else if (userInput == 5)
-                {
-                    Console.WriteLine("Editing Taskbar...\n");
-                    //transparency effects off
                     Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize", "EnableTransparency", "0");
-                    //invisible taskbar off
                     Explorer_Fix.regkey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "TaskbarAcrylicOpacity", "999");
-                    //disable seconds on clock
-                    Explorer_Fix.regkey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "ShowSecondsInSystemClock", "0");
                 }
-                else if (userInput == 6)
+                //Exiting the program.
+                else if (userInput == 0)
                 {
                     Environment.Exit(0);
                 }
                 else
                 {
-                    Console.WriteLine("Unexpected Response, Continuing....");
+                    Console.WriteLine("Invalid Response, Continuing....");
                     Thread.Sleep(2000);
                 }
+                //Restart explorer to apply changes.
                 Explorer_Fix.restart();
+                //Clear console to restart.
                 Console.Clear();
             }
-            
-
-            
         }
     }
 }
